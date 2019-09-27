2c4e9f4addefb62d6a71a78b71b469d8b29f2a6e
Yauheni Ramankou <iromankov2010@gmail.com>
2019-09-27 18:58:14 +0300

    feat: implement three numbers comparison

--- loop_and_conditions.py
+++ loop_and_conditions.py
@@ -8,3 +8,13 @@ if (a == b) or (a == c) or (b == c):
 else:
 	print("ERROR")
 
+
+# one number is the sum of the other two
+d = int(input())
+e = int(input())
+f = int(input())
+
+if (d == e + f) or (e == d + f) or (f == d + e):
+	print("YES")
+else:
+	print("ERROR")
