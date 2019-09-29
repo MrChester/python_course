1ee04859f0e49337fbfa2393508a59dcac574dcd
Yauheni Ramankou <iromankov2010@gmail.com>
2019-09-29 19:38:29 +0300

    feat: add solution for output separation task

---
+++ tasks/lesson5_functions/separate_func.py
@@ -0,0 +1,12 @@
+# Basic output separator with decorator func
+def separate_this(func):
+	def wrapper(param):
+		print("===========")
+		print()
+		func(param)
+		print()
+	return wrapper
+
+@separate_this
+def get_output(param):
+	print(param)
