c3ae0b63a7be7d66249d74bded1e3643e3fd0b69
Yauheni Ramankou <iromankov2010@gmail.com>
2019-10-08 23:59:38 +0300

    feat: add solution for 'triangle class' task

---
+++ tasks/lesson6_work_with_classes/triangle.py
@@ -0,0 +1,10 @@
+class Triangle:
+    def __init__(self, base, height):
+        self.base = base
+        self.height = height
+
+    def area(self):
+        print(int(self.base * self.height / 2))
+
+new_triangle = Triangle(3, 4)
+new_triangle.area()
