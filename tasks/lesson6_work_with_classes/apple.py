2d9ec1f19d3379ef1adcb7356d7852e7fcf1223c
Yauheni Ramankou <iromankov2010@gmail.com>
2019-10-08 23:14:08 +0300

    feat: add solution for apple class task

---
+++ tasks/lesson6_work_with_classes/apple.py
@@ -0,0 +1,15 @@
+class Apple:
+    def __init__(self, color, size, weight, variety):
+        self.color = color
+        self.size = size
+        self.weight = weight
+        self.variety = variety
+
+new_apple = Apple("Green", "Middle", 285, "Honeycrisp")
+print(new_apple.color)
+print(new_apple.size)
+print(new_apple.weight)
+print(new_apple.variety)
+
+new_apple.variety = "Golden Delicious"
+print(new_apple.variety)
