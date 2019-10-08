e5e0bfa2c49a65c6e12cb01341d9c56fba5542b4
Yauheni Ramankou <iromankov2010@gmail.com>
2019-10-08 23:43:08 +0300

    feat: add solution for 'person class' task

---
+++ tasks/lesson6_work_with_classes/person.py
@@ -0,0 +1,12 @@
+class Person:
+    def __init__(self, first_name, last_name, qualification=1):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.qualification = qualification
+
+    def person_info(self):
+        print("First name: {}\nLast name: {}\nQualification: {}".format(
+            self.first_name, self.last_name, self.qualification))
+
+new_person = Person("Yauheni", "Ramankou")
+new_person.person_info()
